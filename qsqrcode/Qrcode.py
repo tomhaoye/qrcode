d8b77e8fca90323744d707ff559d9e77a59529ed
邱奕玮 <qiuyiwei@3ekeji.cn>
2018-10-19 18:23:30 +0800

    struct

---
+++ qsqrcode/Qrcode.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+
+class Qrcode:
+
+    def generate(self):
+        return
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qrcode',
+      version='0.1',
+      description='generate qrcode',
+      url='http://github.com/tomhaoye/qrcode',
+      author='Q',
+      author_email='tomhaoye@gmail.com',
+      license='MIT',
+      packages=['qsqrcode'],
+      zip_safe=False)
