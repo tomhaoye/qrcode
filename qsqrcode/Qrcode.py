c27f1b5a8ae058662a87b04780363586151b4737
tomhaoye <tomhaoye@163.com>
2018-10-24 17:01:58 +0800

    def

--- qsqrcode/Qrcode.py
+++ qsqrcode/Qrcode.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python
+from PIL import Image
 
 
 class Qrcode:
-
-    def init(self, message):
-        return
-
     def resize(self, size):
         return
 
     def generate(self):
+        return
+
+    def __init__(self, message):
+        def draw():
+            def build_locate_sign():
+                return
+
+            def build_time_sign():
+                return
+            return
+
+        def encode():
+            def rs():
+                return
+
+            def mask():
+                return
+
+            def penalty():
+                return
+            return
         return
