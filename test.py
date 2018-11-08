6e76d3ee1662fbc5ea557247c47d5ea8e02b9c1d
tom <tomhaoye@163.com>
2018-11-08 09:01:54 +0800

    new pic

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ qr = Qrcode('https://blog.aikeji.online/2018/10/16/%E4%BA%8C%E7%BB%B4%E7%A0%81%E
 qr.paint('pic/test.jpg').resize(250).generate('testpic/test0.png')
 
 qr = Qrcode('https://blog.aikeji.online/2018/10/16/%E4%BA%8C%E7%BB%B4%E7%A0%81%E7%94%9F%E6%88%90%E5%8E%9F%E7%90%86%E5%8F%8A%E7%BC%96%E7%A0%81%E5%AE%9E%E7%8E%B0/', 'H')
-qr.paint('pic/test.jpg', 1).resize(250).generate('testpic/test1.png')
+qr.paint('pic/testbg.jpg', 1).resize(250).generate('testpic/test1.png')
 
 qr = Qrcode('https://blog.aikeji.online/2018/10/16/%E4%BA%8C%E7%BB%B4%E7%A0%81%E7%94%9F%E6%88%90%E5%8E%9F%E7%90%86%E5%8F%8A%E7%BC%96%E7%A0%81%E5%AE%9E%E7%8E%B0/', 'H')
 qr.colour((199, 29, 69)).resize(250).generate('testpic/test2.png')
